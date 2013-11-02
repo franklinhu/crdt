5c29e72714dc0c10853eacfc33713821a7c38adf
Franklin Hu <franklin@twitter.com>
2013-11-02 10:43:26 -0700

    Actually rename SetLattice to LSet

--- src/main/scala/com/thisisfranklin/crdt/collection/LSet.scala
+++ src/main/scala/com/thisisfranklin/crdt/collection/LSet.scala
@@ -3,14 +3,14 @@ package collection
 
 import com.thisisfranklin.crdt.numeric.MaximumLattice
 
-object SetLattice {
-  def bottom[A] = SetLattice(Set.empty[A])
+object LSet {
+  def bottom[A] = LSet(Set.empty[A])
 }
 
-case class SetLattice[A](value: Set[A]) extends Lattice[Set[A], SetLattice[A]] {
-  def merge(other: SetLattice[A]) = copy(value = value ++ other.value)
+case class LSet[A](value: Set[A]) extends Lattice[Set[A], LSet[A]] {
+  def merge(other: LSet[A]) = copy(value = value ++ other.value)
 
-  def tryCompareTo(other: SetLattice[A]): Option[Int] = {
+  def tryCompareTo(other: LSet[A]): Option[Int] = {
     value.size - other.value.size match {
       case x if x > 0 && (other.value subsetOf value) => Some(1)
       case x if x > 0                                 => None
@@ -22,23 +22,23 @@ case class SetLattice[A](value: Set[A]) extends Lattice[Set[A], SetLattice[A]] {
   }
 
   @Morphism
-  def intersect(other: SetLattice[A]) = copy(value = value intersect other.value)
+  def intersect(other: LSet[A]) = copy(value = value intersect other.value)
 
   @Morphism
-  def map[B](f: A => B): SetLattice[B] = SetLattice(value.map(f))
+  def map[B](f: A => B): LSet[B] = LSet(value.map(f))
 
   @Monotone
   def size: MaximumLattice = MaximumLattice(value.size)
 }
 
-object PositiveSetLattice {
-  def bottom = PositiveSetLattice(Set.empty)
+object PositiveLSet {
+  def bottom = PositiveLSet(Set.empty)
 }
 
-case class PositiveSetLattice(value: Set[Int]) extends Lattice[Set[Int], PositiveSetLattice] {
-  def merge(other: PositiveSetLattice) = copy(value = value ++ other.value)
+case class PositiveLSet(value: Set[Int]) extends Lattice[Set[Int], PositiveLSet] {
+  def merge(other: PositiveLSet) = copy(value = value ++ other.value)
 
-  def tryCompareTo(other: PositiveSetLattice): Option[Int] = {
+  def tryCompareTo(other: PositiveLSet): Option[Int] = {
     value.size - other.value.size match {
       case x if x > 0 && (other.value subsetOf value) => Some(1)
       case x if x > 0                                 => None
